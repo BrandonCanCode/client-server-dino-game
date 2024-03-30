3f870f3558ec1c0c47711466716dbeb76d0480db
brandoncancode <nx00.brandon@gmail.com>
2024-03-30 14:50:04 -0400

    Added input args

--- src/game_client.cpp
+++ src/game_client.cpp
@@ -9,9 +9,9 @@
 
 
 #define PORT "3490" // the port client will be connecting to 
-#define HOSTNAME "localhost"
 #define MAXDATASIZE 64
 
+extern char *HOSTNAME;
 std::queue<char> incoming_queue;
 std::queue<char> outgoing_queue;
 std::mutex i_mtx;
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,30 @@
 #include "game.h"
 #include "player.h"
+#include <string.h>
 
-int main() {
+char *HOSTNAME;
+
+int main(int argc, char **argv) {
 	int max_score = 0;
+
+	if (argc != 2)
+	{
+		printf("Invalid number of input argument %d!=2\n", argc);
+		printf("Usage: ./dinosaur_game <IP4-address>\n");
+		return 1;
+	}
+	else
+	{
+		int length = strlen(argv[1]);
+		if (length < 9)
+		{
+			printf("IP address too small\n");
+			return 1;
+		}
+
+		HOSTNAME = (char*)calloc(sizeof(char), length);
+		strcpy(HOSTNAME, argv[1]);
+	}
 
 	while (1) {
 		int init_status = init();
